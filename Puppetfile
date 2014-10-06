4fb83892326a3a83faae1408f1818ace61b37985
Matteo Abis <gmatteo..abis@gmail.com>
2014-10-06 14:26:13 +0200

    initial version

---
+++ Puppetfile
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+#^syntax detection
+
+forge "https://forgeapi.puppetlabs.com"
+
+mod 'minimum_security', :git => "https://github.com/Enucatl/minimum_security.git"
+mod 'openvpn_as', :git => "https://github.com/Enucatl/openvpn_as.git"
---
+++ site.pp
@@ -0,0 +1,9 @@
+class { 'minimum_security':
+  password => '$1$D5KK5H7a$OCs4OnweVdEe/ll2ZevPd1',
+  ssh_key_location => 'https://github.com/enucatl.keys',
+}
+
+class {'openvpn_as':
+  openvpn_location => 'http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.amd_64.deb',
+  password => '$1$D5KK5H7a$OCs4OnweVdEe/ll2ZevPd1',
+}
